d4a85070fdb3d5f0cd3abca1be333ef5076ed18c
wu-hailong <wuhailong0728@163.com>
2020-01-09 09:10:20 +0800

    扫码完成

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ ReactDOM.render(
 );
 
 serviceWorker.unregister();
-alert(location.href)
 var wx = window.wx
 ;(async function(){
     let result = await get({
@@ -18,7 +17,7 @@ var wx = window.wx
     })
     let { appId, timestamp,nonceStr, signature } = result
     wx.config({
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        // debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId, // 必填，公众号的唯一标识
         timestamp, // 必填，生成签名的时间戳
         nonceStr, // 必填，生成签名的随机串
